53bc9337690b6ac2614efddb69567e5eb7142ecb
XfHua <jasonHuaxf@163.com>
2020-06-17 16:53:18 +0800

    Add files via upload

---
+++ App279/App279/App279/View1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App279
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class View1 : ContentView
+    {
+        public View1()
+        {
+            InitializeComponent();
+
+            var gestureRecognizer = new TapGestureRecognizer();
+
+            gestureRecognizer.Tapped += (s, e) => {
+                if (Command != null && Command.CanExecute(null))
+                {
+                    Command.Execute(null);
+                }
+            };
+
+            this.GestureRecognizers.Add(gestureRecognizer);
+        }
+        // BindableProperty implementation
+        public static readonly BindableProperty CommandProperty =
+            BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(View1), null);
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        // Helper method for invoking commands safely
+        public static void Execute(ICommand command)
+        {
+            if (command == null) return;
+            if (command.CanExecute(null))
+            {
+                command.Execute(null);
+            }
+        }
+    }
+}
